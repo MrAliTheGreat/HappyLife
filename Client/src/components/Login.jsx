9b43bf24dd088c9655dbed7b6fe42de9483fe7b2
Ali Bahari <alibahari007@gmail.com>
2023-08-19 14:51:04 +0330

    Add spinning animations and login button

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,11 +1,24 @@
 import "../styles/login.css"
 
 const login = () => {
+    const animationPaths = [
+        "/animations/Cycling.gif",
+        "/animations/FrogStroke.gif",
+        "/animations/JumpRope.gif",
+        "/animations/Lift.gif",
+        "/animations/PushUp.gif",
+        "/animations/Tennis.gif",
+    ]
+
     return(
         <div className="root">
-            <img className="logo" src="../../public/animations/Run.gif" />
+            {
+                animationPaths.map((path) => <img className="rotating" key={path} src={path}/>)
+            }
+            <img className="logo" src="/animations/Run.gif" />
             <input placeholder="Username" type="text" />           
             <input placeholder="Password" type="text" />
+            <button> Login </button>
         </div>
     )
 }
