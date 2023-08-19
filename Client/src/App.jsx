7fa77d16569659b77f57203dbaba320325daba9b
Ali Bahari <alibahari007@gmail.com>
2023-08-19 20:00:00 +0330

    Add animation for login entrance

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/home"
 import Header from "./components/header"
 
 function App() {
-  const renderOptions = "header"
+  const renderOptions = "login"
 
   return (
     <>
--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -15,10 +15,12 @@ const login = () => {
             {
                 animationPaths.map((path) => <img className={styles.rotating} key={path} src={path}/>)
             }
-            <img className={styles.logo} src="/animations/Run.gif" />
-            <input className={styles.input} placeholder="Username" type="text" />           
-            <input className={styles.input} placeholder="Password" type="text" />
-            <button className={styles.button}> Login </button>
+            <div className={styles.holder}>
+                <img className={styles.logo} src="/animations/Run.gif" />
+                <input className={styles.input} placeholder="Username" type="text" />           
+                <input className={styles.input} placeholder="Password" type="text" />
+                <button className={styles.button}> Login </button>
+            </div>
         </div>
     )
 }
