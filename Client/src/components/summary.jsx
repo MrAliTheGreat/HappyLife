c317a2b1b3966fb0b8bbe971144d1fe49b2f61bd
Ali Bahari <alibahari007@gmail.com>
2023-08-24 00:13:54 +0330

    Add percentage circle to summary component

--- Client/src/components/summary.jsx
+++ Client/src/components/summary.jsx
@@ -1,9 +1,47 @@
 import styles from "../styles/summary.module.css"
 
 const summary = () => {
+    const remainingCal = 2000
+    const totalCal = 6000
+
+    const getBorderStyling = (remainingCal, totalCal) => {
+        const ratioCal = remainingCal / totalCal
+        if(ratioCal < 0.6 && ratioCal >= 0.3){
+            return "5px solid #FFB358"
+        }
+        if(ratioCal < 0.3){
+            return "5px solid #FF5252"
+        }
+        return "5px solid #73FFB1"
+    }
+
+    const getCircleStyling = (remainingCal, totalCal) => {
+        const ratioCal = remainingCal / totalCal
+        if(ratioCal < 0.6 && ratioCal >= 0.3){
+            return `conic-gradient(transparent ${(1 - ratioCal) * 360}deg, #FF8E23 0deg)`
+        }
+        if(ratioCal < 0.3){
+            return `conic-gradient(transparent ${(1 - ratioCal) * 360}deg, #FF2F2F 0deg)`
+        }
+        return `conic-gradient(transparent ${(1 - ratioCal) * 360}deg, #00ff70 0deg)`
+    }
+
+    const getMainGradientStyling = (remainingCal, totalCal) => {
+        const ratioCal = remainingCal / totalCal
+        if(ratioCal < 0.6 && ratioCal >= 0.3){
+            return "linear-gradient(-45deg, #ffc67c, 40%, #da6116)"
+        }
+        if(ratioCal < 0.3){
+            return "linear-gradient(-45deg, #ff1515, 50%, #b6175f)"
+        }
+        return "linear-gradient(-45deg, #13e189, 40%, #008684)"
+    }
+
     return(
-        <div className={styles.main}>
-            Summary
+    <div className={styles.main} style={{background: getMainGradientStyling(remainingCal, totalCal)}}>
+            <div className={styles.countGood} style={{border: getBorderStyling(remainingCal, totalCal), background: getCircleStyling(remainingCal, totalCal)}} >
+                {remainingCal} / {totalCal}
+            </div>
         </div>
     )
 }
