664e460cc0aa4901d0d61f771640bdc66099f32c
Ali Bahari <alibahari007@gmail.com>
2023-08-25 20:27:54 +0330

    Add food and exercise input buttons

--- Client/src/components/header.jsx
+++ Client/src/components/header.jsx
@@ -3,7 +3,13 @@ import styles from "../styles/header.module.css"
 const header = () => {
     return(
         <div className={styles.main}>
+            <div className={`${styles.holder} ${styles.left}`}>
+                <img className={styles.image} src="/images/Dumbbell.png"/>
+            </div>            
             <img className={styles.logo} src="/animations/Run.gif"/>
+            <div className={`${styles.holder} ${styles.right}`}>
+                <img className={styles.image} src="/images/Food.png"/>
+            </div>
         </div>
     )
 }
