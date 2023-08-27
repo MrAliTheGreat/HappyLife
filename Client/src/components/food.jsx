22eb2aefc0c164a84dc9896854b1970e72e684df
Ali Bahari <alibahari007@gmail.com>
2023-08-27 17:12:22 +0330

    Add new food and scale handlers

--- Client/src/components/food.jsx
+++ Client/src/components/food.jsx
@@ -72,14 +72,29 @@ const food = ({ view }) => {
         food: "",
         scale: "",
     })
+    const [newItem, setNewItem] = useState({
+        food: "",
+        scale: "",
+        cal: "",
+    })
     const [amount, setAmount] = useState("")
 
     useEffect(() => {
         setDrop({ food: "", scale: "" })
         setChosen({ food: null, scale: null })
         setSearch({ food: "", scale: "" })
+        setNewItem({ food: "", scale: "", cal: "" })
         setAmount("")
     }, [view])
+
+    const handleChosen = (scale) => {
+        setChosen({...chosen, scale: scale});
+        setDrop({...drop, scale: "hide"});
+        setSearch({...search, scale: ""});
+    }
+
+    // id is set by backend
+    // add mechanisim for clearing chosen
 
     return(
         <div className={styles.main}>
@@ -100,14 +115,22 @@ const food = ({ view }) => {
                 }                
             </div>
             <div className={`${styles.dropdownMenu} ${drop.food === "show" ? styles.dropdownMenuOn : drop.food === "hide" ? styles.dropdownMenuOff : styles.start}`}>
-                <div className={styles.search} >
+                <div className={styles.row} >
                     <div className={styles.holder}>
                         <img className={styles.image} src="/images/Search.png" />
                     </div>
                     <div className={`${styles.holder} ${styles.holderExtend}`}>
                         <input className={styles.input} placeholder="Search Foods" value={search.food} onChange={(e) => setSearch({...search, food: e.target.value})} />
+                    </div>
+                </div>
+                <div className={styles.row} >
+                    <div className={styles.holder}>
+                        <img className={styles.image} src="/images/Missing.png" />
+                    </div>
+                    <div className={`${styles.holder} ${styles.holderExtend}`}>
+                        <input className={styles.input} placeholder="New Food Name?" value={newItem.food} onChange={(e) => {setNewItem({...newItem, food: e.target.value}); setChosen({...chosen, food: {id: 0, name: e.target.value, path: "/images/New.png"} }) } } />
                     </div>                    
-                </div>                    
+                </div>                
                 { foods.filter((food) => food.name.toLocaleLowerCase().includes(search.food.toLocaleLowerCase())).map((food) => {
                     return(
                         <div className={styles.dropdownItem} key={food.id} onClick={() => {setChosen({...chosen, food: food}); setDrop({...drop, food: "hide"}); setSearch({...search, food: ""})} } >
@@ -127,7 +150,7 @@ const food = ({ view }) => {
                     <div className={styles.chosenHolder} >
                         <div className={styles.holder}>
                             <img className={styles.image} src={chosen.scale.path} />
-                        </div>                        
+                        </div>
                         <div className={styles.name}> {chosen.scale.name} </div>
                     </div>
                     :
@@ -137,17 +160,25 @@ const food = ({ view }) => {
                 }                
             </div>
             <div className={`${styles.dropdownMenu} ${styles.secondDropdownMenu} ${drop.scale === "show" ? styles.dropdownMenuOn : drop.scale === "hide" ? styles.dropdownMenuOff : styles.start}`}>
-                <div className={styles.search} >
+                <div className={styles.row} >
                     <div className={styles.holder}>
                         <img className={styles.image} src="/images/Search.png" />
                     </div>
                     <div className={`${styles.holder} ${styles.holderExtend}`}>
                         <input className={styles.input} placeholder="Search Scales" value={search.scale} onChange={(e) => setSearch({...search, scale: e.target.value})} />
                     </div>                    
-                </div>                    
+                </div>
+                <div className={styles.row} >
+                    <div className={styles.holder}>
+                        <img className={styles.image} src="/images/Missing.png" />
+                    </div>
+                    <div className={`${styles.holder} ${styles.holderExtend}`}>
+                        <input className={styles.input} placeholder="New Scale Name?" value={newItem.scale} onChange={(e) => {setNewItem({...newItem, scale: e.target.value}); chosen.food ? setChosen({...chosen, scale: {id: 0, name: e.target.value, path: "/images/New.png"} }) : null } } />
+                    </div>                    
+                </div>                
                 { scales.filter((scale) => scale.name.toLocaleLowerCase().includes(search.scale.toLocaleLowerCase())).map((scale) => {
                     return(
-                        <div className={styles.dropdownItem} key={scale.id} onClick={() => {setChosen({...chosen, scale: scale}); setDrop({...drop, scale: "hide"}); setSearch({...search, scale: ""})} } >
+                        <div className={styles.dropdownItem} key={scale.id} onClick={() => { handleChosen(scale) } } >
                             <div className={styles.holder}>
                                 <img className={styles.image} src={scale.path} />
                             </div>                            
@@ -158,10 +189,14 @@ const food = ({ view }) => {
             </div>
 
             <div className={styles.valueHolder}>
-                <input className={styles.value} placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                <input className={styles.value} placeholder="Amount" value={newItem.food || (newItem.scale && chosen.food) ? 1 : amount} readOnly={newItem.food || (newItem.scale && chosen.food) ? "readonly" : null} onChange={(e) => setAmount(e.target.value)} />
             </div>
 
-            <button className={styles.button}> Submit </button>
+            <div className={styles.valueHolder}>
+                <input className={styles.value} placeholder="Calories" value={newItem.food || (newItem.scale && chosen.food) ? newItem.cal : "Cal from DB"} readOnly={newItem.food || (newItem.scale && chosen.food) ? null : "readonly"} onChange={(e) => setNewItem({...newItem, cal: e.target.value}) } />
+            </div>            
+
+            <button className={styles.button} onClick={() => {console.log(chosen); console.log(newItem)} }> {newItem.food || (newItem.scale && chosen.food) ? "Add To List" : "Submit"} </button>
         </div>
     )
 }
